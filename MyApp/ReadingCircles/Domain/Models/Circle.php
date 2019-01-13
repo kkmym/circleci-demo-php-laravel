f17e947887c0219398c2284733976a181e9a7b37
kkmym <kentarokamiyama@gmail.com>
2019-01-13 21:41:32 +0900

    Circleのテストを追加

--- MyApp/ReadingCircles/Domain/Models/Circle.php
+++ MyApp/ReadingCircles/Domain/Models/Circle.php
@@ -11,31 +11,41 @@ class Circle
 
     public function __construct(Book $readingBook, array $organizers, array $urls = null)
     {
+        //====================================================================
         // $organizers が Member の配列であることを確認
         // 1) array_map
         // 2) Collection
 
-        // 1)
         $isMemberInstance = function($instance) {
             return ($instance instanceof Member);
         };
+        // 1)
+        /*
         if (in_array(false, array_map($isMemberInstance, $organizers))) {
             throw new \Exception('Memberのみ受付');
         }
+        */
+
+        // 2)
+        $c = collect($organizers);
+        if ($c->every($isMemberInstance) == false) {
+            throw new \Exception('Memberのみ受付２');
+        }
+        //====================================================================
 
         $this->readingBook = $readingBook;
         $this->organizers = $organizers;
         $this->urls = $urls;
     }
 
-    public function getBook()
+    public function getReadingBook()
     {
-
+        return $this->readingBook;
     }
 
     public function getOrganaizers()
     {
-
+        return $this->organizers;
     }
 
     public function getOrganizer(int $memberId)
--- tests/unit/ReadingCircles/CircleTest.php
+++ tests/unit/ReadingCircles/CircleTest.php
@@ -9,6 +9,7 @@ use MyApp\ReadingCircles\Domain\Models\BookIsbn;
 use MyApp\ReadingCircles\Domain\Models\Member;
 use MyApp\ReadingCircles\Domain\Models\MemberId;
 use MyApp\ReadingCircles\Domain\Models\MemberLoginId;
+use MyApp\ReadingCircles\Domain\Models\Url;
 
 class CircleTest extends \Codeception\Test\Unit
 {
@@ -21,6 +22,10 @@ class CircleTest extends \Codeception\Test\Unit
     {
         $book = new Book(new BookId(), new BookIsbn('1234567890'), 'テスト');
         $organizer = new Member(new MemberId(), new MemberLoginId('1234abcd'), '名前登録用', '名前表示用');
-        $circle = new Circle($book, [$organizer]);
+        $url = new Url('https://qiita.com/ushironoko/items/a2420cf4a28af56907e5');
+        $circle = new Circle($book, [$organizer], [$url]);
+
+        $this->assertEquals($circle->getReadingBook()->title(), 'テスト');
+        $this->assertEquals(($circle->getOrganaizers())[0]->loginId(), '1234abcd');
     }
 }
